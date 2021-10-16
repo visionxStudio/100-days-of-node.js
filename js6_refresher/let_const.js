891ace64af4902f663c713b6e0b3ff7392ee4449
officialvisionx <manish.rajak2055@gmail.com>
2021-10-16 21:27:19 +0545

    js6 refresher

--- js6_refresher/let_const.js
+++ js6_refresher/let_const.js
@@ -1,2 +1,14 @@
+// Var
+
 var myName = "Manish"
 console.log(myName);
+myName = "Rajak"
+console.log(myName);
+
+
+// Const 
+const perName = "Manish Rajak";
+
+console.log(perName);
+// Cannot re-asign to const value
+// perName = "This will give an error";
